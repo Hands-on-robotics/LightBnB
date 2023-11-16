f295b642809de7cb6df010eb88b0dd949c9e39ca
Hands-on-robotics <handsonevanoff@gmail.com>
2023-11-16 11:46:23 -0800

    refactor getAllReservations in database.js

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -75,7 +75,6 @@ const getUserWithId = function(id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function(user) {
-  console.log(user);
   return pool
     .query(`INSERT INTO users (name, email, password) VALUES
     ($1, $2, $3) RETURNING *;`, [user.name, user.email, user.password])
@@ -95,7 +94,23 @@ const addUser = function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = `SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`;
+
+  return pool
+    .query(query, [guest_id, limit])
+    .then((reservations) => {
+      return reservations.rows || null;
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
 };
 
 /// Properties
